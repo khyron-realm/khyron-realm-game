a5a9486c545cd09211af2d4587e7fec47c071674
sergane13 <59288577+sergane13@users.noreply.github.com>
2021-09-17 23:39:51 +0300

    Improved Training Robots system

--- Assets/Scripts/Robot Related/StoreAndTrainRobots.cs
+++ Assets/Scripts/Robot Related/StoreAndTrainRobots.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,25 +7,27 @@ using UnityEngine.UI;
 
 public class StoreAndTrainRobots : MonoBehaviour
 {
-    //
     [SerializeField] private int _robotsLimit;
     [SerializeField] private RobotsManagerUI _managerUI;
 
-    // 
     [SerializeField] private GameObject _canvasForRobotsInTrain;
+    [SerializeField] private Text _timeText;
 
     // Gameobjects that represent the robots in training from the right side
     private List<GameObject> _robotsInBuildingIcons;
 
     // Robots in training
     public static List<Robot> robotsInTraining;
-
-    // Robots trained
     public static List<Robot> robotsTrained;
+
+    // Total time of building
     public static int totalTime;
 
+    // Check if coroutine started once
     private bool _once = false;
-   
+    private ProgressBar _timeBar;
+    private Text _timeBarText;
+
     private void Awake()
     {
         totalTime = 0;
@@ -37,63 +40,158 @@ public class StoreAndTrainRobots : MonoBehaviour
         _managerUI.OnButtonPressed += AddRobotsToBuild;
     }
 
+
+    // Add and remove methods for Robots to building process
     private void AddRobotsToBuild(Robot robot)
     {
-        if(robotsTrained.Count + robotsInTraining.Count < _robotsLimit)
+        if (robotsTrained.Count + robotsInTraining.Count < _robotsLimit)
         {
             robotsInTraining.Add(robot);
-            totalTime += robot.buildTime;
 
-            GameObject newRobotToCreate = ObjectPoolingTrainingRobots.SharedInstance.GetPooledObjects();
+            TimeIncrement(robot.buildTime);
 
-            if (newRobotToCreate != null)
-            {
-                newRobotToCreate.SetActive(true);
-                _robotsInBuildingIcons.Add(newRobotToCreate);
-                newRobotToCreate.GetComponent<Image>().sprite = robot.icon;
-            }
+            CreateIconInTheRightForRobotInBuilding(robot);
 
             if (robotsInTraining.Count > 0)
             {
-                TrainRobots();
+                StartBuildingRobots();
             }
-        } 
+        }
+    }
+    private void RemoveRobotsToBuild(Robot robot, GameObject robotIcon)
+    {
+        if (robotIcon == _robotsInBuildingIcons[0])
+        {
+            StopCoroutine("BuildRobots");
+
+            robotsInTraining.Remove(robot);
+            _robotsInBuildingIcons.Remove(robotIcon);
+
+            StartCoroutine("BuildRobots");
+        }
+        else
+        {
+            robotsInTraining.Remove(robot);
+            _robotsInBuildingIcons.Remove(robotIcon);
+        }
+     
+        RecalculateTime(_timeText);
+        DezactivateIcon(robotIcon);
+
+        if (_robotsInBuildingIcons.Count > 0)
+        {
+            ActivateDezactivateIconLoadingBar(_robotsInBuildingIcons[0], true);
+        }
+
+        if (robotsInTraining.Count < 1)
+        {
+            StopCoroutine("BuildRobots");
+            _once = false;
+            totalTime = 0;
+        }
     }
 
 
-    private void RemoveRobotsToBuild(Robot robot)
+    // Create icon for the robot in the right
+    private void CreateIconInTheRightForRobotInBuilding(Robot robot)
     {
-        robotsInTraining.Remove(robot);
-        totalTime -= robot.buildTime;
+        GameObject newRobotToCreate = ObjectPoolingTrainingRobots.SharedInstance.GetPooledObjects();
+
+        if (newRobotToCreate != null)
+        {
+            _robotsInBuildingIcons.Add(newRobotToCreate);
+
+            newRobotToCreate.SetActive(true);
+            newRobotToCreate.transform.SetSiblingIndex(_robotsInBuildingIcons.Count - 1);
+            newRobotToCreate.GetComponent<Image>().sprite = robot.icon;
+
+            newRobotToCreate.transform.GetChild(0).GetComponent<Button>().onClick.AddListener(
+                delegate
+                {
+                    RemoveRobotsToBuild(robot, newRobotToCreate);
+                });
+
+            ActivateDezactivateIconLoadingBar(newRobotToCreate, false);
+        }
+    }
+    private static void ActivateDezactivateIconLoadingBar(GameObject newRobotToCreate, bool allocation)
+    {
+        newRobotToCreate.transform.GetChild(1).gameObject.SetActive(allocation);
+        newRobotToCreate.transform.GetChild(2).gameObject.SetActive(allocation);
+    }
+    private void DezactivateIcon(GameObject robotIcon)
+    {
+        robotIcon.SetActive(false);
+        robotIcon.transform.SetSiblingIndex(_robotsInBuildingIcons.Count);
+        robotIcon.transform.GetChild(0).GetComponent<Button>().onClick.RemoveAllListeners();
     }
 
 
-    private void TrainRobots()
+    // Time remained for building robots
+    private static void RecalculateTime(Text _timeText)
     {
-        if(_once == false)
+        totalTime = 0;
+
+        foreach (Robot item in robotsInTraining)
+        {
+            totalTime += item.buildTime;
+        }
+
+        _timeText.text = totalTime.ToString();
+    }
+    private void TimeIncrement(int time)
+    {
+        totalTime += time;
+        _timeText.text = totalTime.ToString();
+    }
+    private void TimeDecrement(int time)
+    {
+        totalTime -= time;
+        _timeText.text = totalTime.ToString();
+    }
+
+
+    // Building process for robots
+    private void StartBuildingRobots()
+    {
+        if (_once == false)
         {
             StartCoroutine("BuildRobots");
             _once = true;
         }
     }
-    
-
     private IEnumerator BuildRobots()
     {
-        for(int i = 0; i < robotsInTraining.Count; i++)
+        for (int i = 0; i < robotsInTraining.Count; i++)
         {
+            if(_robotsInBuildingIcons.Count > 0)
+            {
+                ActivateDezactivateIconLoadingBar(_robotsInBuildingIcons[0], true);
+
+                _timeBarText = _robotsInBuildingIcons[0].transform.GetChild(1).GetComponent<Text>();
+                _timeBar = _robotsInBuildingIcons[0].transform.GetChild(2).GetComponent<ProgressBar>();
+               
+                _timeBar.MaxValue = robotsInTraining[i].buildTime;
+            }
+
             float temp = 0;
-            while(temp < robotsInTraining[i].buildTime)
+
+            while (robotsInTraining[i] != null && temp < robotsInTraining[i].buildTime)
             {
                 temp += 1;
-                totalTime -= 1;
+                _timeBar.CurrentValue = (int)temp;
+                _timeBarText.text = (robotsInTraining[i].buildTime - temp).ToString();
+
+                TimeDecrement(1);
+
                 yield return new WaitForSeconds(1f);
             }
 
             robotsTrained.Add(robotsInTraining[i]);
-            robotsInTraining.RemoveAt(i);
+            robotsInTraining.Remove(robotsInTraining[i]);
 
-            _robotsInBuildingIcons[i].SetActive(false);
+            DezactivateIcon(_robotsInBuildingIcons[i]);
+
             _robotsInBuildingIcons.RemoveAt(i);
             i--;
         }
