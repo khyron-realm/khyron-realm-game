1cd199be9403275b6a61b58aafc47c6a4c787545
Alexandru Grigoras <alex_grigoras_10@yahoo.com>
2021-10-13 12:45:06 +0300

    Added test method calls

--- Assets/Scripts/Networking/Login/LoginHandler.cs
+++ Assets/Scripts/Networking/Login/LoginHandler.cs
@@ -4,11 +4,34 @@ using UnityEngine;
 
 public class LoginHandler : MonoBehaviour
 {
+    private int x = 0;
     public void onClickLogin()
     {
         //LoginManager.Login("gigel", "12345");
-        //LoginManager.AddUser("serban", "parola");
-        UnlimitedPlayerManager.PlayerDataRequest();
-        UnlimitedPlayerManager.ConversionRequest();
+        
+        //LoginManager.AddUser("c", "parola");
+
+        //x = 3;
+        
+        if (x == 0)
+        {
+            LoginManager.Login("c", "parola");
+            x = 1;
+        }
+        else if (x == 1)
+        {
+            //UnlimitedPlayerManager.PlayerDataRequest();
+            
+            //UnlimitedPlayerManager.ConversionRequest();
+            //UnlimitedPlayerManager.CancelConversionRequest();
+            
+            //UnlimitedPlayerManager.UpgradingRequest(1);
+            //UnlimitedPlayerManager.CancelUpgradingRequest();
+            
+            //UnlimitedPlayerManager.BuildingRequest(1, 2);
+            UnlimitedPlayerManager.CancelBuildingRequest(1);
+            
+            //x = 0;
+        }
     }
 }
