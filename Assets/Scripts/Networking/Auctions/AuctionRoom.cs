1f80b39ed74f6ea54b027922d1dbf654eb216068
Alexandru Grigoras <alex_grigoras_10@yahoo.com>
2021-10-31 17:26:25 +0200

    Updated auction room

--- Assets/Scripts/Networking/Auctions/AuctionRoom.cs
+++ Assets/Scripts/Networking/Auctions/AuctionRoom.cs
@@ -2,35 +2,22 @@ using System.Collections.Generic;
 using System.Linq;
 using DarkRift;
 using DarkRift.Server;
+using Networking.Mine;
 
 namespace Networking.Auctions
 {
     /// <summary>
-    ///     
+    ///     Auction room details
     /// </summary>
     public class AuctionRoom : IDarkRiftSerializable
     {
         public ushort Id { get; set; }
         public string Name { get; set; }
-        public byte MinPlayers { get; set; }
         public byte MaxPlayers { get; set; }
-        public bool HasStarted { get; set; }
-        public bool IsVisible { get; set; }
-        public long StartTime { get; set; }
+        public byte PlayerCount { get; set; }
+        public MineData Mine { get; set; }
+        public Bid LastBid { get; set; }
         public long EndTime { get; set; }
-        
-        public List<IClient> Clients = new List<IClient>();
-        public List<Player> PlayerList = new List<Player>();
-        
-        public AuctionRoom(ushort id, string name, bool hasStarted, bool isVisible, long startTime, long endTime)
-        {
-            Id = id;
-            Name = name;
-            HasStarted = hasStarted;
-            IsVisible = isVisible;
-            StartTime = startTime;
-            EndTime = endTime;
-        }
 
         public AuctionRoom()
         {
@@ -40,40 +27,14 @@ namespace Networking.Auctions
         {
             Id = e.Reader.ReadUInt16();
             Name = e.Reader.ReadString();
-            HasStarted = e.Reader.ReadBoolean();
-            IsVisible = e.Reader.ReadBoolean();
-            StartTime = e.Reader.ReadInt64();
+            MaxPlayers = e.Reader.ReadByte();
+            PlayerCount = e.Reader.ReadByte();
+            Mine = e.Reader.ReadSerializable<MineData>();
+            LastBid = e.Reader.ReadSerializable<Bid>();
             EndTime = e.Reader.ReadInt64();
         }
 
         public void Serialize(SerializeEvent e)
-        {
-            e.Writer.Write(Id);
-            e.Writer.Write(Name);
-            e.Writer.Write(HasStarted);
-            e.Writer.Write(IsVisible);
-            e.Writer.Write(StartTime);
-            e.Writer.Write(EndTime);
-        }
-
-        internal bool AddPlayer(Player player, IClient client)
-        {
-            if (PlayerList.Count >= MaxPlayers || HasStarted)
-                return false;
-            
-            PlayerList.Add(player);
-            Clients.Add(client);
-            return true;
-        }
-
-        internal bool RemovePlayer(IClient client)
-        {
-            if (PlayerList.All(p => p.Id != client.ID) && !Clients.Contains(client))
-                return false;
-
-            PlayerList.Remove(PlayerList.Find(p => p.Id == client.ID));
-            Clients.Remove(client);
-            return true;
-        }
+        { }
     }
 }
