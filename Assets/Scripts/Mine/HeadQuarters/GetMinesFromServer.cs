330fffb9814ae2a48b67844203fe2c831d2cd4b0
Alexandru Grigoras <alex_grigoras_10@yahoo.com>
2021-11-04 13:09:41 +0200

    Restructured player and corrected auction and mine methods

--- Assets/Scripts/Mine/HeadQuarters/GetMinesFromServer.cs
+++ Assets/Scripts/Mine/HeadQuarters/GetMinesFromServer.cs
@@ -1,25 +1,61 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Networking.Auctions;
+using Networking.Mines;
+using Networking.Tags;
 
 
 public class GetMinesFromServer : MonoBehaviour
 {
     private void Awake()
     {
-        AuctionsManager.OnReceivedMines += GetMinesForPlayer;
+        MineManager.OnReceivedMines += GetMinesForPlayer;
+        AuctionsManager.OnReceivedOpenRooms += GetOpenRooms;
+        AuctionsManager.OnSuccessfulJoinRoom += SuccessfulJoinRoom;
+        AuctionsManager.OnSuccessfulLeaveRoom += SuccessfulLeaveRoom;
+        AuctionsManager.OnSuccessfulAddBid += SuccessfulAddBid;
+        AuctionsManager.OnFailedAddBid += FailedAddBid;
     }
-    
 
     private void Start()
     {
-        AuctionsManager.GetUserMines();
+        MineManager.GetUserMines();
+        AuctionsManager.GetOpenAuctionRooms();
     }
     
-
-    private void GetMinesForPlayer(List<Networking.Mines.Mine> mineList)
+    private void GetOpenRooms()
     {
+        Debug.Log("Open rooms: " + AuctionsManager.RoomList.Count);
         
+        AuctionsManager.JoinAuctionRoom(1);
+    }
+
+    private void GetMinesForPlayer()
+    {
+        Debug.LogWarning("Mines: " + MineManager.MineList.Count);
+    }
+    
+    private void SuccessfulJoinRoom(List<Player> players)
+    {
+        Debug.LogWarning("Successfully joined room with nr players " + players.Count);
+        Debug.LogWarning("nr scans = " + AuctionsManager.CurrentAuctionRoom.Scans.Length);
+        //AuctionsManager.LeaveAuctionRoom();
+        AuctionsManager.AddBid(1500);
+        AuctionsManager.AddScan(new MineScan("gigel123", 3, 8));
+    }
+    
+    private void SuccessfulLeaveRoom()
+    {
+        Debug.LogWarning("Successfully left room");
+    }
+    
+    private void SuccessfulAddBid()
+    {
+        Debug.LogWarning("Successfully added bid");
+    }
+    
+    private void FailedAddBid()
+    {
+        Debug.LogWarning("Failed to add bid");
     }
 }
--- Assets/Scripts/Networking/Auctions/Player.cs
+++ Assets/Scripts/Networking/Auctions/Player.cs
@@ -7,23 +7,14 @@ namespace Networking.Auctions
     /// </summary>
     public class Player : IDarkRiftSerializable
     {
-        public uint Id { get; set; }
         public string Name { get; set; }
         public bool IsHost { get; private set; }
         
         public Player()
         { }
 
-        public Player(uint id, string name, bool isHost)
-        {
-            Id = id;
-            IsHost = isHost;
-            Name = name;
-        }
-        
         public void Deserialize(DeserializeEvent e)
         {
-            Id = e.Reader.ReadUInt32();
             Name = e.Reader.ReadString();
             IsHost = e.Reader.ReadBoolean();
         }
